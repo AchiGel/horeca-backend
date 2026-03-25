3b05d97bced2f4b9d63a553306992b2158f88d1b
Archil Gelashvili <ar.gelashvili@gmail.com>
2026-03-25 15:33:56 +0400

    hot-fix/fixed body fetching

--- server/models/Article.js
+++ server/models/Article.js
@@ -5,7 +5,7 @@ const ArticleSchema = new mongoose.Schema(
     title: { type: String, required: true },
     slug: { type: String, required: true, unique: true },
     description: { type: String, required: true },
-    body: { type: String, required: true },
+    body: { type: [String], required: true },
     categories: {
       type: [String],
       enum: [
